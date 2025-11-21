7c321af8a587aa9eeb39ebbf0f07f3f4836ae447
alliyafjrn <alliya_fajrin@teknokrat.ac.id>
2025-11-22 01:36:42 +0700

    styling header welcome dan namaUser

--- mobile/styles/home.styles.ts
+++ mobile/styles/home.styles.ts
@@ -3,21 +3,41 @@ import { StyleSheet } from 'react-native';
 export const styles = StyleSheet.create({
   container: {
     flex: 1,
-    paddingTop: 16,
+    paddingTop: 20,
     backgroundColor: '#fff',
+    paddingHorizontal: 16,
   },
 
   searchRow: {
     flexDirection: 'row',
     alignItems: 'center',
-    paddingHorizontal: 16,
-    gap: 8,
+    paddingHorizontal: 8,
+    gap: 4,
   },
 
   searchBar: {
     flex: 1,
-    backgroundColor: '#d9d9d9',
+    backgroundColor: '#F0F0F0',
     borderRadius: 12,
     elevation: 0,
   },
+
+  welcomeContainer: {
+  marginTop: 20,
+  marginBottom: 10,
+  paddingHorizontal: 16,
+},
+
+welcome: {
+  fontSize: 30,
+  color: "#777",
+  marginBottom: 2,
+  textShadowColor: "#FFB347",
+},
+
+nameUser: {
+  fontSize: 20,
+  fontWeight: "700",
+  color: "#FF8C00",
+},
 });
