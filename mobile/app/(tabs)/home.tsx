f849d15c358f69ea0111bed3a4695298f0ec1ad0
alliyafjrn <alliya_fajrin@teknokrat.ac.id>
2025-11-22 02:17:13 +0700

    tambah icon keranjang dan menyusun ulang header agar sebaris dengan keranjang

--- mobile/app/(tabs)/home.tsx
+++ mobile/app/(tabs)/home.tsx
@@ -1,37 +1,48 @@
 import { useState } from "react";
-import { View, Text } from "react-native";
+import { View, Text, Pressable } from "react-native";
 import { Searchbar } from "react-native-paper";
 import { styles } from "@/styles/home.styles";
 import { MaterialIcons } from "@expo/vector-icons";
-
+import { Ionicons } from "@expo/vector-icons";
 
 export default function HomeScreen() {
   const [query, setQuery] = useState("");
 
   return (
     <View style={styles.container}>
-      
       <View style={styles.welcomeContainer}>
-        <Text style={styles.welcome}>Welcome Back !</Text>
-        <Text style={styles.nameUser}>Alliya Fajrin</Text>
+        {/* header welcome */}
+        <View>
+          <Text style={styles.welcome}>Welcome Back !</Text>
+          <Text style={styles.nameUser}>Alliya Fajrin</Text>
+        </View>
+
+        {/* Keranjang */}
+        <Pressable style={styles.cartButton}>
+          <Ionicons name="cart-outline" size={24} color="#333" />
+        </Pressable>
       </View>
 
-
-      {/* searchBar */}
+      {/* Searchbar: */}
       <View style={styles.searchRow}>
-      <Searchbar
-        placeholder="Search products..."
-        value={query}
-        onChangeText={setQuery}
-        style={styles.searchBar}
-        inputStyle={{ fontSize: 15, marginVertical: -4 }}
-        iconColor="#777"
-        placeholderTextColor="#999"
-        // Mic 
-        right={() => (
-          <MaterialIcons name="mic" size={22} color="#555" style={{ marginRight: 10 }} />
-        )}
-      />
+        <Searchbar
+          placeholder="Search products..."
+          value={query}
+          onChangeText={setQuery}
+          style={styles.searchBar}
+          inputStyle={{ fontSize: 15, marginVertical: -4 }}
+          iconColor="#777"
+          placeholderTextColor="#999"
+          // mic
+          right={() => (
+            <MaterialIcons
+              name="mic"
+              size={22}
+              color="#555"
+              style={{ marginRight: 10 }}
+            />
+          )}
+        />
       </View>
     </View>
   );
