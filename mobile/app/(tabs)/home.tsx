acb58fedcf151aa2899dd4e3b5362f2d3ceb525b
alliyafjrn <alliya_fajrin@teknokrat.ac.id>
2025-11-22 01:35:48 +0700

    Tambah Header teks Welcome dan namaUser

--- mobile/app/(tabs)/home.tsx
+++ mobile/app/(tabs)/home.tsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { Pressable, View } from "react-native";
+import { View, Text } from "react-native";
 import { Searchbar } from "react-native-paper";
 import { styles } from "@/styles/home.styles";
 import { MaterialIcons } from "@expo/vector-icons";
@@ -9,14 +9,22 @@ export default function HomeScreen() {
   const [query, setQuery] = useState("");
 
   return (
-    // searchBar
-    <View style={styles.searchRow}>
+    <View style={styles.container}>
+      
+      <View style={styles.welcomeContainer}>
+        <Text style={styles.welcome}>Welcome Back !</Text>
+        <Text style={styles.nameUser}>Alliya Fajrin</Text>
+      </View>
+
+
+      {/* searchBar */}
+      <View style={styles.searchRow}>
       <Searchbar
         placeholder="Search products..."
         value={query}
         onChangeText={setQuery}
         style={styles.searchBar}
-        inputStyle={{ fontSize: 15 }}
+        inputStyle={{ fontSize: 15, marginVertical: -4 }}
         iconColor="#777"
         placeholderTextColor="#999"
         // Mic 
@@ -24,6 +32,7 @@ export default function HomeScreen() {
           <MaterialIcons name="mic" size={22} color="#555" style={{ marginRight: 10 }} />
         )}
       />
+      </View>
     </View>
   );
 }
