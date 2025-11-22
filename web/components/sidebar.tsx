c6dffb053b4e464b468ab1290a24eaa0ba2ca07d
alliyafjrn <alliya_fajrin@teknokrat.ac.id>
2025-11-22 22:51:16 +0700

    add basic sidebar layout structure

---
+++ web/components/sidebar.tsx
@@ -0,0 +1,7 @@
+export default function Sidebar() {
+  return (
+    <div className="w-64 bg-blue-600 min-h-screen text-white p-6">
+      <h1 className="text-2xl font-bold">eProduct</h1>
+    </div>
+  );
+}
