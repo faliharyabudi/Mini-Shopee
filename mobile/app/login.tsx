a2f7086bc38621c3b330a03768e2011c4d9e2346
alliyafjrn <alliya_fajrin@teknokrat.ac.id>
2025-12-16 04:45:22 +0700

    membuat 2 tombol pilihan untuk masuk sebagai 'penjual' atau 'pembeli'

--- mobile/app/login.tsx
+++ mobile/app/login.tsx
@@ -1,16 +1,24 @@
-import { View, Text, StyleSheet, Image } from "react-native";
+import { View, Text, StyleSheet, Image, Pressable } from "react-native";
 
 const logoImage = require('../assets/images/Logo.png');
 
 export default function SplashScreen() {
     return (
-        <View style={styles.container}> 
+        <View style={styles.container}>
             {/* Gambar Logo */}
             <Image source={logoImage} style={styles.logoImg} />
             {/* Teks Logo */}
             <Text style={styles.logoText}>Mini Shop</Text>
             {/* Teks Login Sebagai(customer/seller) */}
             <Text style={styles.loginAs}>Login As</Text>
+            {/* tombol buyyer */}
+            <Pressable style={styles.buttonCustomer}>
+                <Text style={styles.buttonText}>Customer</Text>
+            </Pressable>
+            {/* tombol seller */}
+            <Pressable style={styles.buttonMerchant}>
+                <Text style={styles.buttonText}>Merchant</Text>
+            </Pressable>
         </View>
     );
 }
@@ -28,24 +36,45 @@ const styles = StyleSheet.create({
         fontSize: 80,
         fontWeight: 'bold',
         color: '#ff8c00',
-        textShadowColor: 'rgba(0, 0, 0, 0.75)', 
-        textShadowOffset: { width: 4, height: 6 }, 
-        textShadowRadius: 3, 
+        textShadowColor: 'rgba(0, 0, 0, 0.75)',
+        textShadowOffset: { width: 4, height: 6 },
+        textShadowRadius: 3,
     },
     // Gambar Logo
     logoImg: {
-        // marginTop: ,
-        width: 200, 
-        height: 100, 
-        resizeMode: "contain", 
+        width: 200,
+        height: 100,
+        resizeMode: "contain",
     },
     // beliSebagai
     loginAs: {
-        fontSize: 26,
-        fontWeight: "bold",
-        marginBottom: 200,
-        marginTop: 90,
-
+        fontSize: 20,
+        marginBottom: 20,
+        marginTop: 50,
+        fontWeight: "900",
+    },
+    // Tombol buyer
+    buttonCustomer: {
+        width: "50%",
+        padding: 5,
+        borderRadius: 10,
+        backgroundColor: "#ff8c00",
+        alignItems: "center",
+        marginBottom: 12,
+    },
+    // tombol penjual
+    buttonMerchant: {
+        width: "50%",
+        padding: 5,
+        borderRadius: 10,
+        backgroundColor: "#333",
+        alignItems: "center",
+    },
+    // teks Tombol
+    buttonText: {
+        color: "#fff",
+        fontSize: 32,
+        fontWeight: "600",
     },
 });
 
