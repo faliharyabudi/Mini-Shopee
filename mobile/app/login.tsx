79a571a026c5008105cc64d9dda600dc8f69727d
alliyafjrn <alliya_fajrin@teknokrat.ac.id>
2025-12-16 03:11:46 +0700

    style LogoTeks MiniShop

--- mobile/app/login.tsx
+++ mobile/app/login.tsx
@@ -2,9 +2,26 @@ import { View, Text, StyleSheet } from "react-native";
 
 export default function SplashScreen() {
     return (
-            <Text>MiniShop</Text>
+        <View style={styles.container}>
+            <Text style={styles.logoText}>MiniShop</Text>
+        </View>
     );
 }
+
+// STYLES
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#fff', 
+    },
+    logoText: {
+        fontSize: 80,
+        fontWeight: 'bold',
+        color: '#ff8c00', 
+    }
+});
 
 
 
