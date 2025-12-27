ea92ef0b8e29e295145724680bd2f3be8349acd0
alliyafjrn <alliya_fajrin@teknokrat.ac.id>
2025-12-28 04:11:24 +0700

    nambah endpoint produk dummy di api

--- api/server.js
+++ api/server.js
@@ -1,10 +1,33 @@
 const express = require("express");
 const app = express();
 
-app.use(express.json());
+// dummy data produk
+const products = [
+  {
+    id: 1,
+    name: "Kaos Polos Hitam",
+    price: 75000,
+    stock: 20,
+  },
+  {
+    id: 2,
+    name: "Hoodie Oversize",
+    price: 150000,
+    stock: 10,
+  },
+];
 
+// endpoint root
 app.get("/", (req, res) => {
   res.json({ message: "API Minishop" });
+});
+
+// endpoint products
+app.get("/api/products", (req, res) => {
+  res.json({
+    success: true,
+    data: products,
+  });
 });
 
 app.listen(3001, () => {
