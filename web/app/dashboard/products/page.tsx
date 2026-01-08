4db6ce8a36567c997f6445e87169dd0bb9a54209
alliyafjrn <alliya_fajrin@teknokrat.ac.id>
2026-01-09 01:53:50 +0700

    Tambah botton 'add products' di kanan atas

--- web/app/dashboard/products/page.tsx
+++ web/app/dashboard/products/page.tsx
@@ -5,8 +5,13 @@ export default function ProductsPage() {
       {/* HEADER */}
       <div className="flex items-center justify-between">
         <h1 className="text-2xl font-bold">Products</h1>
+
+        <button className="bg-purple-600 text-white px-4 py-2 rounded-lg hover:bg-purple-700">
+          + Add Product
+        </button>
       </div>
 
-    </div>
+      </div>
+
   );
 }
